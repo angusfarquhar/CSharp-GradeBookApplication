62925287963e5bfcf8025d872ae3c8bca058b528
Angus Farquhar <mr.angus.farquhar@gmail.com>
2020-06-14 17:31:24 +1000

    ranked and standard gradebook

---
+++ GradeBook/GradeBooks/StandardGradeBook.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace GradeBook.GradeBooks
+{
+    public class StandardGradeBook
+    {
+        public StandardGradeBook(string name) : base(name)
+        {
+            this.Type = GradeBookType.Standard;
+        }
+    }
+}
